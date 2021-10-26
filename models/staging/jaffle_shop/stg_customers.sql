1667eb81cac219e8dd1fb99b8278dda2da33297f
Wentao He <wentao@mashey.com>
2021-10-26 21:19:58 +0000

    start-of-polish

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,6 +1,12 @@
-select
-    id as customer_id,
-    first_name,
-    last_name
+with customers as (
 
-from raw.jaffle_shop.customers
+    select
+        id as customer_id,
+        first_name,
+        last_name
+
+    from raw.jaffle_shop.customers
+
+)
+
+select * from customers
