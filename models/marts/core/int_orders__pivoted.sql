d6381c95a5a0e90d54f0b8c5e56efbe4c736e6d3
Wentao He <wentao@mashey.com>
2021-10-27 20:45:02 +0000

    jinja code clean up

--- models/marts/core/int_orders__pivoted.sql
+++ models/marts/core/int_orders__pivoted.sql
@@ -11,10 +11,10 @@ pivoted as (
 
         sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount
 
-        {% if not loop.last %}
+        {%- if not loop.last -%}
             ,
-        {% endif %}
-        {% endfor %}
+        {%- endif %}
+        {% endfor -%}
 
         --sum(case when payment_method = 'bank_transfer' then amount else 0 end) as bank_transfer_amount,
         --sum(case when payment_method = 'coupon' then amount else 0 end) as coupon_amount,
